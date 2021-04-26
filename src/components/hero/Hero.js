95ca471e6a01efef10e79eabb3ff51eb4067c944
bmumz <brandimummery@gmail.com>
2021-04-26 18:04:45 -0400

    Slight changes to Nav & Hero components

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,7 +6,11 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className='hero__container'>
-        <StaticImage src='../../images/slideshow/road.jpg' alt='road' />
+        <StaticImage
+          src='../../images/slideshow/road.jpg'
+          alt='road'
+          className='hero__img'
+        />
 
         <div className='hero__overlay'> </div>
 
