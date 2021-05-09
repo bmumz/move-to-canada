3fd788743c3771171a7cc4e6792e3f70133293b9
bmumz <brandimummery@gmail.com>
2021-05-09 12:41:22 -0400

    Fixing links (mobile displays phone numbers as links)

--- src/components/layout/footer/FooterBanner.js
+++ src/components/layout/footer/FooterBanner.js
@@ -5,15 +5,15 @@ import { useSiteMetadata } from "../../../hooks/use-site-metadata";
 const FooterBanner = () => {
   const { phone } = useSiteMetadata();
   return (
-    <div className='footer__banner'>
-      <h2 className='heading__black '>Get the help you deserve!</h2>
+    <div className="footer__banner">
+      <h3 className="heading__black ">Get the help you deserve!</h3>
       <Banner>
-        <h1 className='heading__bold'>{phone}</h1>
+        <h1 className="heading__bold">{phone}</h1>
       </Banner>
-      <h2 className='heading__center heading__black '>
+      <h3 className="heading__center heading__black ">
         Call now for a free consultation!
-      </h2>
-      <p className='footer__disclaimer'>
+      </h3>
+      <p className="footer__disclaimer">
         We might not be able to handle all types of cases due to expertise and
         other various reasons.
       </p>
