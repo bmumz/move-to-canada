b1a9c83ae251d19989ed0db770395f0e180e7ca1
bmumz <brandimummery@gmail.com>
2021-05-09 14:45:06 -0400

    Adding FAQ feature

---
+++ src/data/faq-data.js
@@ -0,0 +1,144 @@
+const faqData = {
+  general: {
+    heading: `General FAQs`,
+    qa: [
+      {
+        question: `What is The Move to Canada Team?`,
+        answer: `The Move to Canada Team (MTC for short) is your partner for all things related to Canadian
+        immigration and purchasing real estate in Canada.  Our firm is comprised of the best immigration,
+        banking and real estate professionals across the country, each with long histories and proven track
+        records.  We are the only firm in Canada which combines professionals from each vertical, providing
+        a truly comprehensive service that sets us completely apart from the competition.`,
+      },
+      {
+        question: `What does an initial consultation involve?`,
+        answer: `The Move to Canada Team professionals assist hundreds of people around the world each year and
+        we pride ourselves on the in-depth nature of our consultations. Whether your requirement is
+        immigration, buying real estate in Canada, corporate expansion, we will provide extensive
+        information on the process, and conduct a preliminary intake to obtain all of the information we need
+        to best serve you. Forms will be filled out, and meetings will be scheduled for you to meet with the
+        members of the team who will be working with you.`,
+      },
+      {
+        question: `How much does the process cost?`,
+        answer: `Initial consultations with the Move to Canada Team are free of charge. After we speak with you to
+        assess your individual case requirements, you will be introduced to the specific professionals
+        required for your needs. Fees associated with each professional will range based upon the nature
+        and complexity of your case and will be discussed individually with each professional before any
+        services are rendered.`,
+      },
+      {
+        question: `Does The Move to Canada Team accept all cases?`,
+        answer: `In order to provide our clients with the highest level of service, MTC only accepts cases in the areas
+      it specializes. These areas include:`,
+        list: [
+          `Immigrating to Canada and acquiring Permanent Residency in Canada`,
+          `Buying real estate in Canada`,
+          `Corporate expansion and relocations to Canada`,
+          `Corporate law in Canada`,
+          `International taxation in Canada`,
+          `International banking in Canada`,
+        ],
+      },
+      {
+        question: `Is the consultation confidential?`,
+        answer: `All clients, including those attending initial consultations with the Move to Canada Team should feel
+        assured that any information they share with our team will remain strictly confidential.`,
+      },
+    ],
+  },
+  immigration: {
+    heading: `Immigration FAQs`,
+    qa: [
+      {
+        question: `Do I Need to Work with an Immigration Lawyer or Can I Work with a
+              Consultant Instead?`,
+        answer: `Immigration is a very complex field of law where changes can happen overnight, and decisions can
+              be life-changing. Because of this, it is crucial for the person representing the applicant to have a
+              solid knowledge of legal issues. Lawyers have gone through many years of law school and are
+              qualified to find the best solution to suit your needs, while consultants do not have the same level of
+              legal training. Immigration lawyers are experts in this field of law and furthermore, all lawyers on the
+              Move to Canada Team are Certified Specialists in Citizenship and Immigration – designated as the
+              best in the country.`,
+      },
+      {
+        question: `What is a Certified Specialist in Citizenship and Immigration?`,
+        answer: `This designation is the highest possible recognition awarded to immigration lawyers who have
+              maintained exemplary standards of professional practice, specialize in Canadian Citizenship and
+              Immigration, and are recognized as the very best in the country. In Toronto for example, there are
+              only 52 citizenship and immigration lawyers who hold this designation. All immigration lawyers on
+              the Move to Canada Team are Certified Specialists in Citizenship and Immigration.`,
+      },
+      {
+        question: `Do you Guarantee my Application will be Approved?`,
+        answer: `This is difficult to comment upon until your case is assessed. You can be assured that our team of
+              lawyers have likely handled a case similar to yours. Anyone who provides a guarantee of approval
+              ahead of time is misrepresenting themselves. As knowledgeable lawyers, our team provides
+              solutions to clients that have the best chance of success – we will even let you know before you
+              begin the process whether your application has a good chance of being accepted or not. We must
+              confirm the particulars of your case before we can give you the specifics.`,
+      },
+      {
+        question: `How long will it take to approve my Canadian Immigration application?`,
+        answer: `The duration of the application process will depend on the type of application, the circumstances,
+              and how many applications the Government is processing. It must be noted that there is no hard
+              and fast rule for processing times. The Move to Canada Team’s immigration lawyers carefully
+              analyze all documentation provided by clients and prepares professional presentations that save
+              time for visa officers, providing them with an incentive to process your application faster and avoid
+              unnecessary delays.`,
+      },
+
+      {
+        question: `How do I obtain Canadian citizenship after becoming a Permanent
+            Resident?`,
+        answer: `If a person has been physically present in Canada for a period of at least three years within the five
+            years prior to submitting the citizenship application, he/she may qualify for Canadian citizenship.
+            However, the rules for calculating a period of residency are complex and there are exceptions to the
+            standard residency requirement. Therefore, we strongly suggest that you call our office for
+            consultation.`,
+      },
+      {
+        question: `What rights will I have when I am a Canadian Permanent Resident?`,
+        answer: `Canadian Permanent Residents have almost all of the same rights of Canadian citizens. The rights
+              they do not have, are the right to vote, be elected, or hold a Canadian passport. The rights that
+              Permanent residents do have, are the right to free health care, education, employment, grants,
+              social assistance and other benefits depending on their circumstances.`,
+      },
+    ],
+  },
+  realEstate: {
+    heading: `Real Estate FAQs`,
+    qa: [
+      {
+        question: `How Much Does it Cost to Hire a Real Estate Agent in Canada?`,
+        answer: `Real Estate agents in Canada are paid upon closing of a real estate transaction, and are paid a
+                percentage of the transaction value. In Canada, the Seller of the real estate is responsible to pay all
+                real estate agent commissions, both for the seller’s agent, and the buyer’s agent as well. This
+                means that buyers do not pay anything to hire a real estate agent in Canada.`,
+      },
+      {
+        question: `If I Do Not Hire A Real Estate Agent, will I save money on my Purchase?`,
+        answer: `No. When a buyer purchases real estate without a Real Estate Agent, the seller’s Real Estate Agent
+                will be paid both the buyer’s agent commission fee, and the seller’s agent commission fee; this is
+                known as double-ending a deal. There are no savings for a Buyer to purchase real estate without a
+                real estate agent, and in fact this decision will likely cost the buyer more money as they do not have
+                a real estate professional representing them in negotiations.`,
+      },
+      {
+        question: `What are the costs associated with buying real estate in Canada?`,
+        answer: `Real Estate in Canada, like elsewhere in the world, carries a number of costs. In Canada, these
+          costs include:`,
+        list: [
+          `Property Tax`,
+          `Utility costs (electricity, natural gas, water)`,
+          `Maintenance costs for general upkeep`,
+          `Mortgage payments in the case that financing was required for the purchase`,
+          `Land Transfer Tax (depending on where in Canada the real estate is purchased)`,
+          `The Non Resident Speculation Tax (depending on where in Canada the real estate is
+            purchased), for buyers without a Canadian Permanent Residency or Canadian Citizenship.`,
+        ],
+      },
+    ],
+  },
+};
+export default faqData;
