cf87c48a975653004f50afbd1c804c257b1ff74c
bmumz <brandimummery@gmail.com>
2021-06-26 23:51:20 -0400

    add service submenu

--- src/components/layout/nav/LinkBuilder.js
+++ src/components/layout/nav/LinkBuilder.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
+import servicesData from "../../../data/services-data";
+import { serviceRoutes } from "../../../routes";
 
 const LinkBuilder = ({ collection }) =>
   collection.map((page, index) => (
-    <Link to={page.path} key={index}>
-      {page.title}
-    </Link>
+    <div
+      className={page.path === "/services" ? "navbar__services--anchor" : null}
+      key={index}
+    >
+      <Link to={page.path} className="navbar__main">
+        {page.title}
+      </Link>
+
+      {page.path === "/services" && (
+        <div className="navbar__services">
+          <ul>
+            {serviceRoutes.map((s, i) => (
+              <Link to={s.path} key={i}>
+                <li>{s.page}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   ));
 
 export default LinkBuilder;
--- src/data/services-data.js
+++ src/data/services-data.js
@@ -3,19 +3,16 @@ const servicesData = {
   blurb: `From individuals and
   small local businesses, to the world’s largest companies, we support all of
   your immigration and real estate requirements.`,
-  images: {
-    firstRow: {
-      home: "Buying a Home",
-      immigration: "Personal/Family Immigration",
-      businessRelocation: "Business Relocation",
-      banking: "International Banking",
-    },
-    secondRow: {
-      mortgage: "Mortgage Financing",
-      realEstate: "Real Estate Law",
-      taxation: "International Taxation",
-      other: "Other Services",
-    },
+  pages: {
+    home: "Buying a Home",
+    immigration: "Personal/Family Immigration",
+    employeeRelocation: "Employee Relocation",
+    mortgage: "Mortgage Financing",
+    taxation: "International Taxation",
+    realEstate: "Real Estate Law",
+    businessRelocation: "Business Relocation",
+    banking: "International Banking",
+    other: "Additional Services",
   },
   details: `We are committed to customer service and personalized attention to ensure each client receives custom tailored solutions that fit their needs.`,
 };
